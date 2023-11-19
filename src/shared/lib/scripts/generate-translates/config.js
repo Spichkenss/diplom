bcce1438faea91c81c06c3c513368d0e0929ee02
spichkenss <spichkens.2002@mail.ru>
2023-11-19 17:14:28 +0300

    Translations generator rewritten in js

--- src/shared/lib/scripts/generate-translates/config.js
+++ src/shared/lib/scripts/generate-translates/config.js
@@ -1,22 +1,25 @@
-"use strict";
 /* eslint-disable no-shadow, no-unused-vars */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sourceLanguage = exports.localesMap = exports.translationsDirPath = exports.LogType = exports.colors = void 0;
-// Console colors
-exports.colors = {
+
+module.exports = {
+  // Console colors
+  colors: {
     reset: "\x1b[0m",
     fgBlack: "\x1b[30m",
     bgGreen: "\x1b[42m",
     bgRed: "\x1b[41m",
-};
-var LogType;
-(function (LogType) {
-    LogType[LogType["success"] = 0] = "success";
-    LogType[LogType["error"] = 1] = "error";
-})(LogType || (exports.LogType = LogType = {}));
-exports.translationsDirPath = "../../../../../public/translations";
-exports.localesMap = {
+  },
+
+  LogType: {
+    success: "success",
+    error: "error",
+  },
+
+  translationsDirPath: "../../../../../public/translations",
+
+  localesMap: {
     ru: "Russian",
     en: "English",
+  },
+
+  sourceLanguage: "Russian",
 };
-exports.sourceLanguage = "Russian";
